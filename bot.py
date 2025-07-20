cbd268d31a48693332778b337d5b3a4f7692bd55
Junui Hong <jha357@sfu.ca>
2025-07-19 22:26:15 -0700

    base code updated

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv() # .env file read
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='>>', intents=intents)
+    
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f"🔁 Synced {len(synced)} slash command(s)")
+    except Exception as e:
+        print(f"Sync failed: {e}")
+    print(f'{bot.user} is ON')
+
+
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
+
+import asyncio
+asyncio.run(main())
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@ Discord Bot
 ==
 Creation Date: 250719
 Latest Modified: 250719
- 
+
